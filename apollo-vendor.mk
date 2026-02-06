ba59a1b41d4a07dc103fcb9236c3d7c593a2d10e
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2026-02-06 19:08:57 +0500

    Apollo: Import vendor.xiaomi.hardware.campostproc@1.0-impl.so
    - For leica to work on Apollo.

--- apollo-vendor.mk
+++ apollo-vendor.mk
@@ -585,6 +585,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.qteeconnector@1.0-impl \
     vendor.qti.hardware.sensorscalibrate@1.0-impl \
     vendor.qti.hardware.soter@1.0-impl \
+    vendor.xiaomi.hardware.campostproc@1.0-impl \
     vendor.xiaomi.hardware.touchfeature@1.0-impl \
     jcos_nq_client \
     lib-imscmservice \
