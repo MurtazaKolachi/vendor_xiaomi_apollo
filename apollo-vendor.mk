6744840e50a4a0126215b0e8f6041c5d5c0ed7f1
Michael Bestas <mkbestas@lineageos.org>
2026-07-28 04:19:44 +0500

    Apollo: Import and Patch qccsyshal to load versioned libprotobuf

--- apollo-vendor.mk
+++ apollo-vendor.mk
@@ -1018,6 +1018,7 @@ PRODUCT_PACKAGES += \
     libwfduibcsrcinterface \
     vendor.qti.ImsRtpService-V1-ndk \
     vendor.qti.diaghal@1.0 \
+    vendor.qti.hardware.qccsyshal@1.2-halimpl \
     vendor.qti.hardware.wifidisplaysession@1.0 \
     vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     vendor.qti.imsrtpservice@3.0 \
